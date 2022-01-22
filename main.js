cf2523945956afc6bbc9781a63ed76206813fed5
yassinerahhaui <yassinerahhaoui12@gmail.com>
2022-01-22 22:17:34 +0100

    add project {not completed.}

---
+++ main.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", () => {
+  autoSlide();
+})
+
+function autoSlide() {
+  setInterval(() => {
+    slide(getItemActiveIndex() + 1);
+  }, 3000);
+}
+
+function slide(toIndex) {
+  const itemsArray = Array.from(document.querySelectorAll('.carousel_item'));
+  const itemActive = document.querySelector('.carousel_item__active');
+
+  if (toIndex >= itemsArray.length) {
+    toIndex = 0;
+  }
+  const newItemActive = itemsArray[toIndex]
+
+  newItemActive.classList.add("carousel_item__pos_next");
+  setTimeout(() => {
+    newItemActive.classList.add("carousel_item__next");
+    itemActive.classList.add("carousel_item__next");
+  }, 10);
+
+  newItemActive.addEventListener("transitionend", () => {
+    itemActive.className = "carousel_item";
+    newItemActive.className = "carousel_item carousel_item__active";
+  }, { once: true })
+}
+
+
+function getItemActiveIndex() {
+  const itemsArray = Array.from(document.querySelectorAll('.carousel_item'));
+  const itemActive = document.querySelector('.carousel_item__active');
+  const itemActiveIndex = itemsArray.indexOf(itemActive);
+  return itemActiveIndex;
+}
